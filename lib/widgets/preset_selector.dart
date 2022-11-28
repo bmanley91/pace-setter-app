5a432d8b0b007ab3411296609ea4ea795e8fe0e0
Brian Manley <brian.manley91@gmail.com>
2022-11-27 21:11:13 -0500

    Fixed dropdown arrow position

--- lib/widgets/preset_selector.dart
+++ lib/widgets/preset_selector.dart
@@ -17,6 +17,8 @@ class _PresetSelectorState extends State<PresetSelector> {
     return DropdownButton(
         hint: const Text('Select a preset'),
         value: _dropdownValue,
+        isExpanded: true,
+        icon: const Icon(Icons.arrow_downward),
         items: list.map<DropdownMenuItem<String>>((String value) {
           return DropdownMenuItem<String>(
             value: value,
