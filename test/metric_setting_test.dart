d3144c700ca73cbee2a0042a8d5920a442ac0798
Brian Manley <brian.manley91@gmail.com>
2023-01-07 22:59:00 -0500

    Add test around flipping metric setting

---
+++ test/metric_setting_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pace_tracker_app/redux/metric_setting_reducer.dart';
+import 'package:pace_tracker_app/redux/metric_setting_store.dart';
+import 'package:pace_tracker_app/redux/metric_state_action.dart';
+import 'package:redux/redux.dart';
+
+void main() {
+  test('emitting a flip action will flip the value of the store', () {
+    Store<bool> testStore = Store(
+      metricSettingReducer,
+      initialState: false,
+    );
+    testStore.dispatch(MetricStateActions.flip);
+    expect(testStore.state, true);
+  });
+}
