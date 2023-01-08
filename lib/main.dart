a4fae02881764e40d71478963b2095acbdff03fc
Brian Manley <brian.manley91@gmail.com>
2023-01-08 16:56:31 -0500

    Persist redux state

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:pace_tracker_app/redux/app_state.dart';
 import 'package:pace_tracker_app/redux/metric_setting_reducer.dart';
 import 'package:pace_tracker_app/widgets/pace_checker_app.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_persist/redux_persist.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
 void main() async {
+  // If we don't do this the app just doesn't load
+  WidgetsFlutterBinding.ensureInitialized();
+
   final persistor = Persistor<AppState>(
-    storage: FileStorage(File('state.json')), // Or use other engines
-    serializer:
-        JsonSerializer<AppState>(AppState.fromJson), // Or use other serializers
+    storage: FlutterStorage(),
+    serializer: JsonSerializer<AppState>(AppState.fromJson),
   );
 
   final initialState = await persistor.load();
--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 class AppState {
   final bool metricUnitsEnabled;
 
@@ -6,8 +8,12 @@ class AppState {
   AppState copyWith({bool? metricUnitsEnabled}) => AppState(
       metricUnitsEnabled: metricUnitsEnabled ?? this.metricUnitsEnabled);
 
-  static AppState fromJson(dynamic json) =>
-      AppState(metricUnitsEnabled: json['metricUnitsEnabled']);
+  static AppState fromJson(dynamic json) {
+    debugPrint('Loaded json: $json');
+    return json != null
+        ? AppState(metricUnitsEnabled: json["metricUnitsEnabled"])
+        : AppState();
+  }
 
   dynamic toJson() => {'metricUnitsEnabled': metricUnitsEnabled};
 }
