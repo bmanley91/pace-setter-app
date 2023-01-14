acac8c27a91f946c973496983a0b688171239514
Brian Manley <brian.manley91@gmail.com>
2023-01-13 20:16:56 -0500

    Added calculator fields to store

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -2,11 +2,26 @@ import 'package:flutter/widgets.dart';
 
 class AppState {
   final bool metricUnitsEnabled;
+  final String distance;
+  final String time;
+  final String pace;
 
-  AppState({this.metricUnitsEnabled = false});
+  AppState(
+      {this.distance = '',
+      this.time = '',
+      this.pace = '',
+      this.metricUnitsEnabled = false});
 
-  AppState copyWith({bool? metricUnitsEnabled}) => AppState(
-      metricUnitsEnabled: metricUnitsEnabled ?? this.metricUnitsEnabled);
+  AppState copyWith(
+          {bool? metricUnitsEnabled,
+          String? distance,
+          String? time,
+          String? pace}) =>
+      AppState(
+          metricUnitsEnabled: metricUnitsEnabled ?? this.metricUnitsEnabled,
+          distance: distance ?? this.distance,
+          time: time ?? this.time,
+          pace: pace ?? this.pace);
 
   static AppState fromJson(dynamic json) {
     debugPrint('Loaded json: $json');
