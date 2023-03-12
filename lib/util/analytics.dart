648f2bb520623c2c0253bcfbd74f5e40d3983679
Brian Manley <brian.manley91@gmail.com>
2023-03-12 11:23:55 -0400

    Remove debug from analytics client

--- lib/util/analytics.dart
+++ lib/util/analytics.dart
@@ -7,15 +7,10 @@ class AnalyticsClient {
 
   static final AnalyticsClient instance = AnalyticsClient._privateConstructor();
 
-  logPageLoad() async {
-    const environment = kDebugMode ? 'debug' : 'production';
-    debugPrint('Logging page load for environment: $environment');
-    await FirebaseAnalytics.instance.logEvent(
-      name: 'page-load',
-      parameters: {
-        'environment': kDebugMode ? 'debug' : 'production',
-      },
-    );
-    debugPrint('Finished logging page load');
-  }
+  logPageLoad() async => await FirebaseAnalytics.instance.logEvent(
+        name: 'page-load',
+        parameters: {
+          'environment': kDebugMode ? 'debug' : 'production',
+        },
+      );
 }
